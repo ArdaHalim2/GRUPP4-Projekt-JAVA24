970370cd33918b632db98822e6e7bc43364e91cb
ArdaHalim2 <arda.halim@iths.se>
2024-12-18 11:01:26 +0100

    JPAUtil & persistence.xml update

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -10,34 +10,15 @@ CREATE TABLE student
 CREATE TABLE test
 (
     testId           INT AUTO_INCREMENT PRIMARY KEY,
-    testName         VARCHAR(255) NOT NULL,
     testCategory     VARCHAR(100),
     testMaxScore     INT,
-    testStudentScore INT,
+    testStudentScore INT DEFAULT 0,
     testDate         DATE,
     test_studentId   INT, -- Foreign key som kopplar till student
     FOREIGN KEY (test_studentId) REFERENCES student (studentId)
 );
 
--- Skapa tabell för country
-CREATE TABLE country
-(
-    countryId             INT AUTO_INCREMENT PRIMARY KEY,
-    countryName           VARCHAR(255) UNIQUE NOT NULL,
-    countryCapital_cityID INT, -- Foreign key som kopplar till city
-    countryPopulation     BIGINT
-);
-
--- Skapa tabell för lake
-CREATE TABLE lake
-(
-    lakeId         INT AUTO_INCREMENT PRIMARY KEY,
-    lakeName       VARCHAR(255) NOT NULL,
-    lake_countryId INT, -- Foreign key som kopplar till country
-    FOREIGN KEY (lake_countryId) REFERENCES country (countryId)
-);
-
--- Skapa tabell för city
+-- Skapa tabell för city (städer)
 CREATE TABLE city
 (
     cityId         INT AUTO_INCREMENT PRIMARY KEY,
@@ -46,12 +27,86 @@ CREATE TABLE city
     FOREIGN KEY (city_countryId) REFERENCES country (countryId)
 );
 
--- Lägga till en relation mellan country och city för huvudstad
+-- Skapa tabell för country (länder)
+CREATE TABLE country
+(
+    countryId      INT AUTO_INCREMENT PRIMARY KEY,
+    countryName    VARCHAR(255) UNIQUE NOT NULL,
+    countryCapitaL INT, -- Foreign key som kopplar till city (städer)
+    FOREIGN KEY (countryCapitaL) REFERENCES city (cityId)
+);
+
+-- Skapa tabell för lake (sjöar)
+CREATE TABLE lake
+(
+    lakeId         INT AUTO_INCREMENT PRIMARY KEY,
+    lakeName       VARCHAR(255) NOT NULL,
+    lake_countryId INT, -- Foreign key som kopplar till country
+    FOREIGN KEY (lake_countryId) REFERENCES country (countryId)
+);
+
+INSERT INTO city (cityName, city_countryId)
+VALUES ('Paris', 1),
+       ('London', 2),
+       ('Madrid', 3),
+       ('Rom', 4),
+       ('Aten', 5),
+       ('Amsterdam', 6),
+       ('Brussels', 7),
+       ('Bern', 8),
+       ('Warszawa', 9),
+       ('Oslo', 10),
+       ('Lisabon', 11),
+       ('Prag', 12),
+       ('Stockholm', 13),
+       ('Budapest', 14),
+       ('Helsingfors', 15);
+
+
+INSERT INTO country (countryName, countryCapitaL)
+VALUES ('Frankrike', 1),
+       ('Storbritannien', 2),
+       ('Spanien', 3),
+       ('Italien', 4),
+       ('Grekland', 5),
+       ('Nederländerna', 6),
+       ('Belgien', 7),
+       ('Schweiz', 8),
+       ('Polen', 9),
+       ('Norge', 10),
+       ('Portugal', 11),
+       ('Tjeckien', 12),
+       ('Sverige', 13),
+       ('Ungern', 14),
+       ('Finland', 15);
+
+INSERT INTO country (countryName, countryCapitaL)
+VALUES ('Frankrike', 1),
+       ('Storbritannien', 2),
+       ('Spanien', 3),
+       ('Italien', 4),
+       ('Grekland', 5),
+       ('Nederländerna', 6),
+       ('Belgien', 7),
+       ('Schweiz', 8),
+       ('Polen', 9),
+       ('Norge', 10),
+       ('Portugal', 11),
+       ('Tjeckien', 12),
+       ('Sverige', 13),
+       ('Ungern', 14),
+       ('Finland', 15);
+
+
 ALTER TABLE country
     ADD CONSTRAINT fk_country_capital
         FOREIGN KEY (countryCapital_cityID) REFERENCES city (cityId);
 
 -- Skapa index för att förbättra sökningar på viktiga kolumner
-CREATE INDEX idx_studentId ON test(test_studentId);
-CREATE INDEX idx_countryId ON lake(lake_countryId);
-CREATE INDEX idx_city_countryId ON city(city_countryId);
+CREATE INDEX idx_studentId ON test (test_studentId);
+CREATE INDEX idx_countryId ON lake (lake_countryId);
+CREATE INDEX idx_city_countryId ON city (city_countryId);
+
+CREATE INDEX idx_country_name ON country (countryName)
+CREATE INDEX idx_city_name ON city (cityName)
+CREATE INDEX idx_city_name ON lake (lakeName)
